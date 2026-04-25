c539396f2796bd4dcd9f7994adb472f65372d10d
saamailala <schemodanova@internet.ru>
2026-04-25 11:50:10 +0300

    Update Запомни порядок.py

--- Запомни порядок.py
+++ Запомни порядок.py
@@ -1,6 +1,7 @@
 import sys
 from PySide6.QtWidgets import *
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QFont
 
 
 class MemoryOrderGame(QMainWindow):
@@ -12,6 +13,18 @@ class MemoryOrderGame(QMainWindow):
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         self.main_layout = QVBoxLayout(self.central_widget)
+
+        self.welcome = QWidget()
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignCenter)
+
+        title = QLabel("Запомни порядок")
+        title.setAlignment(Qt.AlignCenter)
+        title.setFont(QFont("Georgia", 56))
+        layout.addWidget(title)
+
+        self.welcome.setLayout(layout)
+        self.main_layout.addWidget(self.welcome)
 
 
 if __name__ == "__main__":
