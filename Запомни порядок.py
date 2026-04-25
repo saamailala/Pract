b2c86bd2a260b99ae4a8d728cf33070980862592
saamailala <schemodanova@internet.ru>
2026-04-25 11:49:49 +0300

    Update Запомни порядок.py

--- Запомни порядок.py
+++ Запомни порядок.py
@@ -2,11 +2,17 @@ import sys
 from PySide6.QtWidgets import *
 from PySide6.QtCore import Qt
 
+
 class MemoryOrderGame(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Запомни порядок")
         self.setWindowState(Qt.WindowMaximized)
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.main_layout = QVBoxLayout(self.central_widget)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
