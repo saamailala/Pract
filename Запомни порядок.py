e830a153dd2b6c3dfa19cf86aca6b7b8bb783aad
saamailala <schemodanova@internet.ru>
2026-05-16 12:04:18 +0300

    Update Запомни порядок.py

--- Запомни порядок.py
+++ Запомни порядок.py
@@ -23,6 +23,52 @@ class MemoryOrderGame(QMainWindow):
         title.setFont(QFont("Georgia", 56))
         layout.addWidget(title)
 
+        self.start_btn = QPushButton("Начать игру")
+        self.start_btn.setFont(QFont("Georgia", 16))
+        self.start_btn.setFixedSize(220, 60)
+        self.start_btn.setCursor(Qt.PointingHandCursor)
+        self.start_btn.setStyleSheet("""
+            QPushButton {
+                background-color: #E8B4BC;
+                color: white;
+                border: none;
+                border-radius: 30px;
+            }
+            QPushButton:hover { background-color: #D9A9B4; }
+        """)
+        layout.addWidget(self.start_btn, alignment=Qt.AlignCenter)
+
+        self.rules_btn = QPushButton("Правила")
+        self.rules_btn.setFont(QFont("Georgia", 14))
+        self.rules_btn.setFixedSize(150, 50)
+        self.rules_btn.setCursor(Qt.PointingHandCursor)
+        self.rules_btn.setStyleSheet("""
+            QPushButton {
+                background-color: #E8B4BC;
+                color: white;
+                border: none;
+                border-radius: 25px;
+            }
+            QPushButton:hover { background-color: #D9A9B4; }
+        """)
+        layout.addWidget(self.rules_btn, alignment=Qt.AlignCenter)
+
+        self.exit_btn = QPushButton("Выход")
+        self.exit_btn.setFont(QFont("Georgia", 14))
+        self.exit_btn.setFixedSize(150, 50)
+        self.exit_btn.setCursor(Qt.PointingHandCursor)
+        self.exit_btn.setStyleSheet("""
+            QPushButton {
+                background-color: #E8B4BC;
+                color: white;
+                border: none;
+                border-radius: 25px;
+            }
+            QPushButton:hover { background-color: #D9A9B4; }
+        """)
+        self.exit_btn.clicked.connect(self.close)
+        layout.addWidget(self.exit_btn, alignment=Qt.AlignCenter)
+
         self.welcome.setLayout(layout)
         self.main_layout.addWidget(self.welcome)
 
